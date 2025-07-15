8563765086f86bacedb257534972b90ce0e213ab
Zihan XU <zihan.xu@hpmicro.com>
2025-07-15 09:53:39 +0800

    [iar] add lost header file for iar
    - add lost header file for IAR toolchain
    
    Signed-off-by: Zihan XU <zihan.xu@hpmicro.com>

---
+++ boot/hpmicro/include/sys/types.h
@@ -0,0 +1,17 @@
+#ifndef _TYPES_H
+#define _TYPES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef int mode_t;
+typedef long off_t;
+typedef char *caddr_t;
+typedef int ssize_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
